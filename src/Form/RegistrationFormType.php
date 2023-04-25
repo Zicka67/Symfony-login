58a68623a99ec7cd3cca92cd66cae74062a23d8f
Zicka67 <art.book@hotmail.fr>
2023-04-25 11:09:02 +0200

    Ajout du pseudo dans le registerForm + de la classe TextType etc..

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,20 +4,25 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            //On rajoute dans le builder les champ qu'on veut, ici pseudo, et sa classe PAS OUBLIER d'ajouter la classe en use
+            ->add('pseudo', TextType::class)
+            ->add('email', EmailType::class)
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
@@ -26,22 +31,31 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
+            //Changer en RepeatedType pour créer 2 Champs identiques pour la verif du MDP
+            ->add('plainPassword', RepeatedType::class, [
+                //Pour indiquer que le champ ne sera pas stocker en DB
+                'mapped' => false,
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password fields must match.',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options'  => ['label' => 'Password'],
+                'second_options' => ['label' => 'Repeat Password'],
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
+                // 'mapped' => false,
+                // 'attr' => ['autocomplete' => 'new-password'],
+                // 'constraints' => [
+                //     new NotBlank([
+                //         'message' => 'Please enter a password',
+                //     ]),
+                //     new Length([
+                //         'min' => 6,
+                //         'minMessage' => 'Your password should be at least {{ limit }} characters',
+                //         // max length allowed by Symfony for security reasons
+                //         'max' => 4096,
+                //     ]),
+                // ],
             ])
         ;
     }
